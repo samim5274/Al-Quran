d6e158e7d8e1d090c977479fc8026327dbbddeb5
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2022-04-05 15:30:43 +0600

    update

---
+++ app/src/main/java/com/example/al_quran/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.example.al_quran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar Progressbar;
+    private int progess;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Progressbar = findViewById(R.id.ProgressBarId);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+                AppStart();
+            }
+        });
+        thread.start();
+    }
+
+    private void doWork(){
+        for (progess=20; progess<100; progess=progess+20)
+        {
+            try {
+                Thread.sleep(1000);
+                Progressbar.setProgress(progess);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void AppStart(){
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
